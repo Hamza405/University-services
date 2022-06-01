a034f981c698789315f581f1c341d55f80913b6f
hamza <hamzash863@gmail.com>
2022-06-01 19:27:48 +0300

    Improve dashboard navbar

--- resources/views/templates/navbar.blade.php
+++ resources/views/templates/navbar.blade.php
@@ -1,5 +1,8 @@
 
 <ul class="ttr-header-navigation" style="position: absolute;right: 5rem;">
+    <li>
+        <a href="{{ url('/adminDash') }}" class="ttr-material-button ttr-submenu-toggle" style="padding:0px 8px;">القسم : <span style="padding:0px 8px; color:white"> {{Auth::user()->section}} </span> </a>
+    </li>
     @if(Auth::user()->role == 'Admin')
         <li>
             <a href="{{ url('/home') }}" class="ttr-material-button ttr-submenu-toggle">الرئيسية</a>
@@ -8,5 +11,6 @@
     <li>
         <a href="{{ url('/logout') }}" class="ttr-material-button ttr-submenu-toggle" style="padding-right:0px">تسجيل خروج</a>
     </li>
+    
 
 </ul>
